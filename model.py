bf975e9b65704c85fb891601d9dfdab3ef0787a1
Geeksten <temozex@gmail.com>
2015-10-28 16:20:35 -0700

    First commit added shell of modelpy with class user. Added readme.

---
+++ model.py
@@ -0,0 +1,66 @@
+"""Models and database functions for Alerts project."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+# This is the connection to the SQLite database; we're getting this through
+# the Flask-SQLAlchemy helper library. On this, we can find the `session`
+# object, where we do most of our interactions (like committing, etc.)
+
+db = SQLAlchemy()
+
+
+##############################################################################
+# Model definitions
+
+class User(db.Model):
+    """User of Alerts website."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    fname = db.Column(db.String(64), nullable=False)
+    lname = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    phone = db.Column(db.Varchar(15), nullable=False)
+    zipcode = db.Column(db.String(5), nullable=False)
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<User user_id=%s fname = %s email=%s>" % (self.user_id, self.fname, self.email)
+
+
+class Userreport(db.Model):
+    """User report on Alerts website."""
+
+    pass
+
+
+class Reportsymptom(db.Model):
+    pass
+
+
+class Symptoms(db.Model):
+    pass
+
+##############################################################################
+# Helper functions
+
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our SQLite database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///alerts.db'
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print "Connected to DB."

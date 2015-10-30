9485f669975167d70e1ef52fa533a1443a7ee1f2
Geeksten <temozex@gmail.com>
2015-10-29 17:45:27 -0700

    added class state, statesrecall, fdarecall

--- model.py
+++ model.py
@@ -18,17 +18,16 @@ class User(db.Model):
     __tablename__ = "users"
 
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String(64), nullable=False)
     fname = db.Column(db.String(64), nullable=False)
     lname = db.Column(db.String(64), nullable=False)
-    email = db.Column(db.String(64), nullable=False)
     password = db.Column(db.String(64), nullable=False)
-    phone = db.Column(db.Varchar(15), nullable=False)
-    zipcode = db.Column(db.String(5), nullable=False)
 
     def __repr__(self):
         """Provide helpful representation when printed."""
 
-        return "<User user_id=%s fname = %s email=%s>" % (self.user_id, self.fname, self.email)
+        return "<User user_id=%s fname = %s lname = %s email=%s>" % (
+            self.user_id, self.fname, self.lname, self.email)
 
 
 class Userreport(db.Model):
@@ -56,18 +55,19 @@ class Userreport(db.Model):
 
 class Reportsymptom(db.Model):
     """Provides a way to connect the userreports and symptoms tables"""
+
     __tablename__ = "reportsymptoms"
 
     reportsym_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     urep_id = db.Column(db.Integer, db.ForeignKey('userreports.urep_id'))
     sym_id = db.Column(db.Integer, db.ForeignKey('symptoms.sym_id'))
 
-    # Define relationship to user
+    # Define relationship to userreport
     userreport = db.relationship("Userreport",
                                  backref=db.backref("reportsymptoms",
                                                     order_by=reportsym_id))
 
-    # Define relationship to sympyom
+    # Define relationship to symptom
     symptom = db.relationship("Symptom",
                               backref=db.backref("reportsymptoms",
                                                  order_by=reportsym_id))
@@ -91,13 +91,77 @@ class Symptom(db.Model):
     def __repr__(self):
         """Provide helpful representation when printed."""
 
-        return "<Symptom sym_id=%s sym_name=%s description>" % (
+        return "<Symptom sym_id=%s sym_name=%s description=%s>" % (
             self.sym_id, self.sym_name, self.description)
+
+
+class State(db.Model):
+    """States on Alerts website."""
+
+    __tablename__ = "states"
+
+    state_id = db.Column(db.String(2), primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    latitude = db.Column(db.String(64), nullable=False)
+    longitude = db.Column(db.String(64), nullable=False)
+
+    def __repr__(self):
+
+        return "<State state_id=%s name=%s>" % (
+            self.state_id, self.name)
+
+
+class Staterecall(db.Model):
+    """Provides a way to connect the states table to the fdarecalls table."""
+
+    __tablename__ = "statesrecalls"
+
+    state_recallid = db.Column(db.String(20), primary_key=True)
+    state_id = db.Column(db.String(2), db.ForeignKey('states.state_id'))
+    fdarep_id = db.Column(db.String(2), db.ForeignKey('fdarecalls.recall_id'))
+
+    # Define relationship to state
+    state = db.relationship("State",
+                            backref=db.backref("statesrecalls",
+                                               order_by=state_recallid))
+
+    # Define relationship to fdarecall
+    fdarecall = db.relationship("FDArecall",
+                                backref=db.backref("staterecalls",
+                                                   order_by=state_recallid))
+
+    def __repr__(self):
+
+        return "<State state_recallid=%s state_id=%s fdarep_id=%s>" % (
+            self.state_recallid, self.state_id, self.fdarep_id)
+
+
+class Fdarecall(db.Model):
+    """Provides a way to connect the states table to the fdarecalls table."""
+
+    __tablename__ = "fdarecalls"
+
+    recall_id = db.Column(db.Integer(20), autoincrement=True, primary_key=True)
+    status = db.Column(db.String(20), nullable=False)
+    recall_date = db.Column(db.DateTime, nullable=False)
+    recall_firm = db.Column(db.String(50), nullable=False)
+    prod_type = db.Column(db.String(20), nullable=False)
+    prod_desc = db.Column(db.String(200), nullable=False)
+    dist_pattern = db.Column(db.String(200), nullable=False)
+    recall_city = db.Column(db.String(50), nullable=False)
+    recall_state = db.Column(db.String(2), nullable=False)
+    recall_country = db.Column(db.String(2), nullable=False)
+    recall_reason = db.Column(db.String(200), nullable=False)
+    date_reported = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+
+        return "<FDArecall recall_id=%s status=%s recall_date=%s recall_firm =%s prod_type=%s prod_desc=%s dist_pattern=%s recall_city=%s recall_state=%s recall_country=%s recall_reason=%s date_reported=%s>" % (
+            self.recall_id, self.status, self.recall_date, self.recall_firm, self.prod_type, self.prod_desc, self.dist_pattern, self.recall_city, self.recall_state, self.recall_country, self.recall_reason, self.date_reported)
+
 
 ##############################################################################
 # Helper functions
-
-
 def connect_to_db(app):
     """Connect the database to our Flask app."""
 

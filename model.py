6fa34b598d928fef123de7a374f30b47967fea2a
Geeksten <temozex@gmail.com>
2015-10-30 13:15:46 -0700

    requirementsdottxt

--- model.py
+++ model.py
@@ -89,7 +89,7 @@ class Symptom(db.Model):
     description = db.column(db.String(200), nullable=False)
 
     def __repr__(self):
-        """Provide helpful representation when printed."""
+        """Providesde helpful representation when printed."""
 
         return "<Symptom sym_id=%s sym_name=%s description=%s>" % (
             self.sym_id, self.sym_name, self.description)
@@ -116,7 +116,7 @@ class Staterecall(db.Model):
 
     __tablename__ = "statesrecalls"
 
-    state_recallid = db.Column(db.String(20), primary_key=True)
+    state_recallid = db.Column(db.String(20), autoincrement=True, primary_key=True)
     state_id = db.Column(db.String(2), db.ForeignKey('states.state_id'))
     fdarep_id = db.Column(db.String(2), db.ForeignKey('fdarecalls.recall_id'))
 
@@ -156,8 +156,14 @@ class Fdarecall(db.Model):
 
     def __repr__(self):
 
-        return "<FDArecall recall_id=%s status=%s recall_date=%s recall_firm =%s prod_type=%s prod_desc=%s dist_pattern=%s recall_city=%s recall_state=%s recall_country=%s recall_reason=%s date_reported=%s>" % (
-            self.recall_id, self.status, self.recall_date, self.recall_firm, self.prod_type, self.prod_desc, self.dist_pattern, self.recall_city, self.recall_state, self.recall_country, self.recall_reason, self.date_reported)
+        return """<FDArecall recall_id=%s status=%s recall_date=%s recall_firm =%s
+                    prod_type=%s prod_desc=%s dist_pattern=%s recall_city=%s
+                    recall_state=%s recall_country=%s recall_reason=%s
+                    date_reported=%s>""" % (
+            self.recall_id, self.status, self.recall_date,
+            self.recall_firm, self.prod_type, self.prod_desc,
+            self.dist_pattern, self.recall_city, self.recall_state,
+            self.recall_country, self.recall_reason, self.date_reported)
 
 
 ##############################################################################

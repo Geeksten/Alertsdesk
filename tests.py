f0748360e7e06ba9e077a8dcc5c59fa1de998917
Geeksten <temozex@gmail.com>
2015-11-30 15:19:21 -0800

    added test for homepage

---
+++ tests.py
@@ -0,0 +1,18 @@
+import unittest
+from server import index, app
+
+# integration tests vs function tests
+
+
+class MyFlaskIntegrationsTests(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_index(self):
+        result = self.client.get("/")
+        self.assertIn("here", result.data)
+        self.assertEqual(result.status_code, 200)
+        print dir(result)
+
+if __name__ == "__main__":
+    unittest.main()

71df630fc876852a32a5c1d606ba2341a3eb6eb6
Geeksten <temozex@gmail.com>
2015-12-08 20:40:14 -0800

    cleaned up jinja for userreport html

--- static/illnessmarkers_map.js
+++ static/illnessmarkers_map.js
@@ -3,7 +3,7 @@ var map;
 
 function initialize() {
     var mapOptions = {
-        zoom: 10
+        zoom: 15
     };
     map = new google.maps.Map(
             document.getElementById('illnessmarkers-map'),
